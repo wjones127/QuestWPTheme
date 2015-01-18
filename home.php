cfe3826883f0ac2ad3432c7c8bd8dc17889acaa5
Will Jones <thefreethinker127@gmail.com>
2015-01-17 20:23:46 -0800

    Added the news category, testing the second loop.

--- home.php
+++ home.php
@@ -7,7 +7,7 @@
 <div class="center-container">
 <div id="slider" class="gallery-container swipe">
 <div class="featured-gallery swipe-wrap">
-<?php query_posts( 'category_name=featured&posts_per_page=5' ); ?>
+<?php query_posts( 'category_name=slider&posts_per_page=5' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
     <div class="slide">
 	<h2 class="headline featured-headline">
@@ -20,7 +20,11 @@
 	    <img src="<?php echo '$feat_image' ?>">
 	</a>
     </div>
-<?php endwhile; ?>
+<?php 
+# Adds the ID of the post to a list so we know which posts to not duplicate in later sections
+$do_not_duplicate[] = $post->ID;
+endwhile; 
+?>
 </div><!-- End .featured-gallery -->
 
 <!-- Indicator for where the slider is -->
@@ -32,8 +36,54 @@
 </div> <!-- End center-container -->
 
 
+<div class="main-wrapper center-container">
+<div class="section group">
 
-
+<!-- First 5 columns -->
+<div id="featured-news" class="col span5">
+<!-- News Stories -->
+<h3><a href="category.html">Latest News</a></h3>
+<ul class="story-list">
+<?php 
+rewind_posts();
+$news = array(
+    'post__not_in' => $do_not_duplicate,
+    'posts_per_page' => 5,
+    'category_name' => news,
+)
+query_posts($news);
+if (have_posts()) :
+$post = $posts[0]; $c = 0;
+while (have_posts()) : the_post();
+# For the first post, have the image, if there is one
+$c++;
+if ( $c == 1) :
+?>
+<li class="story">
+    <h4><a href="<?php echo the_permalink() ?>">
+	<?php echo the_title() ?>
+    </a></h4>
+    <?php
+    # If there is a feature image for the post, show it
+    if ( has_post_thumbnail( $post_id ) ) : 
+     $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+    ?>
+	<a href="<?php the_permalink(); ?>">
+	    <img src="<?php echo '$feat_image' ?>">
+	</a>
+    <?php endif ?>
+    <p class="byline">by <?php the_author(); ?> | <?php the_date(); ?></p>
+    <?php the_excerpt(); ?>
+</li>
+<?php else; ?>
+<li class="story">
+    <h4><a href="<?php echo the_permalink() ?>">
+	<?php echo the_title() ?>
+    </a></h4>
+    <p class="byline">by <?php the_author(); ?> | <?php the_date(); ?></p>
+</li>
+<?php endif;
+endwhile; ?>
 
 
 
@@ -43,7 +93,7 @@
 
 <!-- SLIDER JAVASCRIPT -->
 
-<script src="Swipe/swipe.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/Swipe/swipe.js"></script>
 <script>
 window.counter = {
     element: document.getElementsByClassName('counter')[0],
