de03d9c14f29b02babd4205101d2c09b36f04a8c
Will Jones <thefreethinker127@gmail.com>
2015-01-20 17:30:06 -0800

    Fixed missing semi-colons

--- functions.php
+++ functions.php
@@ -4,11 +4,11 @@ function responsive_feature_image($id, $class) {
 	$medium = wp_get_attachment_image_src( get_post_thumbnail_id($id), 'medium')[0];
 	$large = wp_get_attachment_image_src( get_post_thumbnail_id($id), 'large')[0];
 	$alt_text = get_post_meta($id, '_wp_attachment_image_alt', true);
-    echo '<img src="'.$small.'" '
-    echo 'class="'.$class.'" '
-    echo 'sizes="(min-width: 1200) 1200px, 100%" '
-    echo 'srcset="'.$small.' 480w, '.$medium.' 720w, '.$large.'1200w"'
-    echo 'alt="'.$alt_text.'">'
+    echo '<img src="'.$small.'" ';
+    echo 'class="'.$class.'" ';
+    echo 'sizes="(min-width: 1200) 1200px, 100%" ';
+    echo 'srcset="'.$small.' 480w, '.$medium.' 720w, '.$large.'1200w"';
+    echo 'alt="'.$alt_text.'">';
 }
 
 ?>
